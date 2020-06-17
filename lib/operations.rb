c1cb41334ac98248a6afaf06b0b6a27c5252119c
Priscilla Leyva <pleyva.co89@gmail.com>
2020-06-17 01:40:40 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,15 @@
-def unsafe?(speed="50")
-if speed < 60
- puts "unsafe"
-else 
-  puts "safe"
+def unsafe?(speed)
+ if speed > 60 || speed < 40
+  true
+ else 
+  false
+ end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : false
 end
 	
 
